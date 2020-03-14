2359443b0261bc7940eabf68c03fa0e996c13402
lyubomyr83 <lyubomyr83@mail.ru>
2020-03-14 18:01:23 +0300

    14.03,2020

--- 21.py
+++ 21.py
@@ -10,52 +10,71 @@ move = 1
 more = 'yes'
 
 while more == 'yes':
-    dealler_card = cards.pop()
-    dealler_points += dealler_card
-    
-    
-    your_cards = []
     if move == 1:
+        dealler_card = cards.pop()
+        dealler_points += dealler_card
+    
+    
+        your_cards = []
+    
         for i in range(2):
             your_card = cards.pop()
             your_cards.append(your_card)
             your_points += your_card
     else:
         your_card = cards.pop()
-        your_cards.append(your_card)
         your_points += your_card
   
   
-
+  
     print(f'''
+        ============
+        POINTS:
+        Nigga: {dealler_points}
+        You: {your_points} 
+        ============
+
+        Nigga got cards with face value {dealler_card} 
+        ''')
+        
+
+    if move == 1:
+        print(f'You got cards with face value {your_cards[0]} and {your_cards[1]} ')
+    else:
+        print(f'You got cards with face value {your_cards} ' )
+
+    if your_points & dealler_points < 22:
+        if your_points == 21:
+            more = 'no'
+        else:
+            more = input('more? yes or no\n')
+    
+        move += 1 
+    else:
+        print('over drow')
+        break  
+else:
+    while dealler_points < 18:
+        dealler_card = cards.pop()
+        dealler_points += dealler_card
+        
+        print(f'''
     ============
     POINTS:
     Nigga: {dealler_points}
     You: {your_points} 
     ============
+
+    Nigga got cards with face value {dealler_card} 
     ''')
 
-    if move == 1:
-        print(f'You got cards with face value {your_cards[0]} and {your_cards[1]} ')
+    if your_points == dealler_points:
+        print('draw')
+    elif your_points > dealler_points:
+        print('you win')
+    elif your_points == 21:
+        print('you win')
+    elif dealler_points > 21:
+        print('you win')
     else:
-        print(f'You got cards with face value {your_cards[0]} ' )
-    if your_points & dealler_points < 22:
-        if your_points or dealler_points == 21:
-            if your_points == 21:
-                print('You win')
-            if dealler_points == 21:
-                print('You lose')
-        elif your_points == dealler_points:
-            print('draw')
-            break
-        elif your_points < dealler_points:
-            print('You lose')
-            break
-        elif your_points > dealler_points:
-            more = input('Else more? Yes or no\n' )
-            if not more:
-                if your_points > dealler_points:
-                    print('You win')
-                    break
-    
-    move += 1 
+        print('you lose')
